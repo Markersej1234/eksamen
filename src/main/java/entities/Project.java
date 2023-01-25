b0a608e1965e581d8c350d44aa78fe172eec5c31
Mark Gonzales Nielsen <cph-mn651@cphbusiness.dk>
2023-01-25 09:57:39 +0100

    Entities

---
+++ src/main/java/entities/Project.java
@@ -0,0 +1,68 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+
+@Entity
+@NamedQuery(name = "Project.deleteAllRows", query = "DELETE from Project ")
+public class Project {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+
+    @ManyToMany(mappedBy = "projectList")
+    private List<Developer> developerList;
+
+    @OneToMany(mappedBy = "project")
+    private List<ProjectHours> projectHoursList;
+
+    public List<Developer> getDevelopers() {
+        return developerList;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Developer> getDeveloperList() {
+        return developerList;
+    }
+
+    public void setDeveloperList(List<Developer> developerList) {
+        this.developerList = developerList;
+    }
+
+    public List<ProjectHours> getProjectHoursList() {
+        return projectHoursList;
+    }
+
+    public void setProjectHoursList(List<ProjectHours> projectHoursList) {
+        this.projectHoursList = projectHoursList;
+    }
+}
